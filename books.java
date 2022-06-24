0c9a11e5a7d1f2e0c3a21236017099b94502321e
Shahil Nara <40553434+SNara4@users.noreply.github.com>
2022-06-24 17:18:58 +0530

    Add files via upload

---
+++ books.java
@@ -0,0 +1,8 @@
+package mypack;
+
+public class books {
+    int price;
+    String name;
+   public books(){name="java"; price=100;}
+    public void show(){System.out.println(name+" costs"+price);}
+}
